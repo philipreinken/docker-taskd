fa476a0585ef89a7353c446825577ac8bc87c2a0
Philip Gatzka <philip.gatzka@mailbox.org>
2019-03-09 11:40:12 +0100

    Make server configurable

--- Dockerfile
+++ Dockerfile
@@ -1,14 +1,15 @@
 FROM ubuntu:18.04
 
 ENV DEBIAN_FRONTEND="noninteractive"
-ENV TASKDDATA="/home/taskd/data"
-ENV TASKDGIT="/home/taskd/taskd.git"
-ENV TASKDCERTS="$TASKDDATA/certs"
+ENV TASKDHOME="/home/taskd"
+ENV TASKDDATA="$TASKDHOME/data"
+ENV TASKDGIT="$TASKDHOME/taskd.git"
+ENV TASKDPKI="$TASKDDATA/pki"
 ENV BUILD_DEPENDENCIES="python git libgnutls28-dev uuid-dev build-essential cmake ca-certificates"
 
-RUN useradd -m taskd && mkdir -p $TASKDCERTS && \
+RUN useradd -m taskd && \
   apt update && apt upgrade -y && \
-  apt install -y --no-install-recommends ${BUILD_DEPENDENCIES} gnutls-bin && \
+  apt install -y --no-install-recommends ${BUILD_DEPENDENCIES} gnutls-bin gettext-base && \
   update-ca-certificates && \
   git clone --depth=1 https://github.com/GothenburgBitFactory/taskserver.git $TASKDGIT && \
   cd $TASKDGIT && \
@@ -17,27 +18,19 @@ RUN useradd -m taskd && mkdir -p $TASKDCERTS && \
   make && \
   cd test && make && ./run_all && cd .. && \
   make install && \
+  mkdir -p $TASKDDATA && \
+  cp -r $TASKDGIT/pki $TASKDPKI && \
   apt remove -y --autoremove ${BUILD_DEPENDENCIES} && \
   apt-get clean && \
-  chown -R taskd:taskd $TASKDDATA
+  chown -R taskd:taskd $TASKDHOME
 
-COPY entrypoint.sh /entrypoint.sh
-RUN chmod +x /entrypoint.sh
-
-USER taskd
+COPY entrypoint.sh $TASKDHOME/entrypoint.sh
+COPY vars.template $TASKDPKI/vars.template
 
 EXPOSE 53589
 
+USER taskd
+
 WORKDIR $TASKDDATA
 
-RUN taskd init && \
-  taskd config server 0.0.0.0:53589 && \
-  taskd config --force client.cert $TASKDCERTS/client.cert.pem && \
-  taskd config --force client.key $TASKDCERTS/client.key.pem && \
-  taskd config --force server.cert $TASKDCERTS/server.cert.pem && \
-  taskd config --force server.key $TASKDCERTS/server.key.pem && \
-  taskd config --force server.crl $TASKDCERTS/server.crl.pem && \
-  taskd config --force ca.cert $TASKDCERTS/ca.cert.pem && \
-  taskd config --force log /dev/stdout
-
-ENTRYPOINT ["/entrypoint.sh"]
+ENTRYPOINT ["/home/taskd/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,38 +1,61 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-USER="user"
-ORG="default"
-INITIALISED="$(egrep -r 'CERTIFICATE' $TASKDCERTS | wc -l)" || true
+# Ensure these are set before continuing
+TASKDHOME=$TASKDHOME
+TASKDDATA=$TASKDDATA
+TASKDGIT=$TASKDGIT
+TASKDPKI=$TASKDPKI
 
-generate_server_certs() {
-  cd $TASKDCERTS && cp $TASKDGIT/pki/vars $TASKDCERTS/vars
+# Config
+TASKD_LOG=${TASKD_LOG:="/dev/stdout"}
+TASKD_PID_FILE=${TASKD_PID_FILE:="$TASKDHOME/taskd.pid"}
+TASKD_SERVER=${TASKD_SERVER:="0.0.0.0:53589"}
 
-  $TASKDGIT/pki/generate.ca >/dev/null 2>&1
-  $TASKDGIT/pki/generate.server >/dev/null 2>&1
-  $TASKDGIT/pki/generate.crl >/dev/null 2>&1
+# Export the necessary certificate vars, fall back to default values
+export TASKD_SEC_PARAM=${SEC_PARAM:="high"}
+export TASKD_EXPIRATION_DAYS=${EXPIRATION_DAYS:="365"}
+export TASKD_ORGANIZATION=${ORGANIZATION:='"Göteborg Bit Factory"'}
+export TASKD_CN=${CN:="localhost"}
+export TASKD_COUNTRY=${COUNTRY:="SE"}
+export TASKD_STATE=${STATE:='"Västra Götaland"'}
+export TASKD_LOCALITY=${LOCALITY:='"Göteborg"'}
+
+pki_vars_override() {
+    envsubst < $TASKDPKI/vars.template > $TASKDPKI/vars
 }
 
-add_default_user() {
-  taskd add org default >/dev/null 2>&1
-  taskd add user $ORG $USER | egrep key > $TASKDCERTS/user-key
+create_certificates() {
+    pki_vars_override && (cd $TASKDPKI && ./generate)
 }
 
-print_user_info() {
-  echo "user: $USER"
-  echo "key: $(cat $TASKDCERTS/user-key | head -n1 | awk -F': ' '{print $2}')"
-  echo "group: $ORG"
-  echo ""
+taskd_init() {
+    taskd init
+    create_certificates
+    
+    # Configure all generated certificates
+    for cert in $TASKDPKI/*.pem; do
+        cert="$(basename $cert)"
+        config="${cert%.pem}"
+        
+        # In case of the api.* certificate, the config key needs to be client.*
+        if [[ $cert =~ ^api ]]; then
+            config="${config/#api/client}"
+        fi;
+
+        taskd config --force $config "$TASKDPKI/$cert"
+    done;
+
+    taskd config --force log $TASKD_LOG
+    taskd config --force pid.file $TASKD_PID_FILE
+    taskd config --force server $TASKD_SERVER
+
+    # Display config
+    taskd config
 }
 
-init() {
-  generate_server_certs
-  add_default_user
-  print_user_info
-}
-
-if [[ (( $INITIALISED < 1 )) ]]; then
-  init
+if [[ ! -e "$TASKDDATA/config" ]]; then
+    taskd_init
 fi
 
 taskd server --data $TASKDDATA
